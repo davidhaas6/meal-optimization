5f1b26b96d84a7a34352b13adf26d18f3f67dcaa
David Haas <davidhaas6@gmail.com>
2023-01-23 01:40:07 -0600

    paramaterized optimization constraints

--- optimize.py
+++ optimize.py
@@ -2,38 +2,36 @@ import pulp
 import json
 
 
-def optimize_food_counts(foods):
+def optimize_food_counts(foods, constraints):
 	foods = foods.interpolate()  # fill NaN via interpolation
+
+	
+	#TODO: can we add a constraint on leftover servings? something like recipe_serving_amt - X > 0
+	#			maybe like diet_model += (1/foods.servings).dot(X) <= pulp.lpSum(X) + 2
+	#TODO: add num_ingredient column
+	#TODO: add num_ingredients_in_pantry column
 
 	diet_model = pulp.LpProblem("The_Diet_Problem")
 	X_dict = pulp.LpVariable.dict("x_%s", foods.descriptor, lowBound = 0, upBound = 2, cat=pulp.LpInteger)
 	X = list(X_dict.values())
 
-	#TODO: can we add a constraint on leftover servings? something like recipe_serving_amt - X > 0
-	#TODO: add num_ingredient column
-	#TODO: add num_ingredients_in_pantry column
-	#TODO: add time_to_cook column
+	# apply the constraints
+	for varName, varConstraints in constraints.iterrows():
+		if varName not in foods.columns: continue
+		if varConstraints['min'] > 0:
+			diet_model += foods[varName].dot(X) >= varConstraints['min']	
+		if varConstraints['max'] > 0:
+			diet_model += foods[varName].dot(X) <= varConstraints['max']
+			
+	diet_model += (pulp.lpSum(X) <= 4, "Maximum_total_meals")
+	diet_model += (pulp.lpSum(X) >= 0, "Minimum_total_meals") 
 
-	diet_model += foods.calories.dot(X) >= 2100
-	diet_model += foods.calories.dot(X) <= 2800
-	diet_model += foods.protein.dot(X) >= 70
-	diet_model += foods.fat.dot(X) >= 70
-	diet_model += foods.fat.dot(X) <= 120
-	# diet_model += foods.Cholesterol.dot(X) <= 1
-	diet_model += foods.sodium.dot(X) <= 3
-	diet_model += foods.carbohydrates.dot(X) >= 250
-	diet_model += foods.fiber.dot(X) >= 15
-	# diet_model += foods.recipe_serving_amt.dot(X) >= 300
-	diet_model += foods.Breakfast.dot(X) >= 1
-	diet_model += foods.Lunch.dot(X) >= 1
-	diet_model += foods.Dinner.dot(X) >= 1
-	diet_model += foods.Snack.dot(X) <= 1
-	diet_model += (pulp.lpSum(X) <= 5, "Maximum_total_meals")
-	diet_model += (pulp.lpSum(X) >= 2, "Minimum_total_meals") 
-	diet_model.solve()
-
-	# print(diet_model)
+	try:
+		diet_model.solve()
+	except pulp.apis.core.PulpSolverError as e:
+		print(e)
+		foods['numMeals'] = [0 for _ in range(len(foods))]
+		return foods
 
 	foods['numMeals'] = [x.value() for x in X]
-
 	return foods
--- test.py
+++ test.py
@@ -1,17 +1,54 @@
 import pandas as pd
 import optimize
 
-df = pd.read_csv('data/bb_nutrition.csv',index_col=0)
+# load and preprocess our data
+dataset = pd.read_csv('data/nutrition.csv', index_col=0)
 bad_rows = '|'.join(['best-ever-avocado-dip'])
-df = df[~df.descriptor.str.contains(bad_rows)]
+dataset = dataset[~dataset.descriptor.str.contains(bad_rows)]
+# print(dataset.head(3))
 
-df = df.sample(frac=0.75,axis=0)
-df = optimize.optimize_food_counts(df)
+# incorporate user preferences
+constraints = pd.DataFrame(index=dataset.columns.unique())
+constraints['min'] = {
+    'calories': 2100,
+    'protein': 70,
+    'fat': 50,
+    'carbohydrates': 50,
+    'fiber': 10,
+    'Breakfast': 1,
+    'Lunch': 1,
+    'Dinner': 1,
+}
+constraints['max'] = {
+    'calories': 2800,
+    'protein': 150,
+    'fat': 120,
+    'carbohydrates': 400,
+    'sodium': 4,
+    'totalTime': 60,
+    'cookTime': 60,
+    'Breakfast': 2
+}
+# print(constraints)
 
-mealplan = df[df.numMeals > 0]
-mealplan['leftover_servings'] = mealplan.servings - mealplan.numMeals
+# optimize
+days = []
+for i in range(1):
+    sample = dataset.sample(frac=.75, axis=0)
+    mealplan = optimize.optimize_food_counts(sample, constraints)
+    mealplan = mealplan[mealplan.numMeals > 0]
+    print(f"\nDaily meal plan {i+1}")
+    days.append(', '.join(mealplan.descriptor))
+    print(', '.join(mealplan.descriptor))
 
-total_macros = mealplan.iloc[:,1:-2].multiply(mealplan.numMeals,axis=0)
+for i, day in enumerate(days):
+    print(f"\nDaily meal plan {i+1}:")
+    print(day.replace('-', ' '))
+
+# postprocess and print
+mealplan = mealplan[mealplan.numMeals > 0]
+mealplan['leftover_servings'] = mealplan.servings.subtract(mealplan.numMeals)
+total_macros = mealplan.iloc[:, 1:-2].multiply(mealplan.numMeals, axis=0)
 total_macros['descriptor'] = mealplan.descriptor
 
 print("\n\nMeal Plan:")
@@ -19,4 +56,3 @@ print(mealplan)
 # print("\n\nMacros:")
 # print(total_macros)
 print('\n', total_macros.sum())
-
